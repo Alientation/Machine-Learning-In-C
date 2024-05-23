0750b273c201a8d6d7f39a23a0ff037359b1764a
Bill <billma.texas@gmail.com>
2024-05-23 14:05:15 -0500

    last of the useful tests for now

--- tests/util/matrix_test.cpp
+++ tests/util/matrix_test.cpp
@@ -101,5 +101,209 @@ TEST(matrix, matrix_multiply_identity) {
 }
 
 TEST(matrix, matrix_multiply) {
+    double a0[2] = {1,2};
+    double *array1[1] = {a0};
+    matrix_t *m1 = matrix_constructor(1, 2, (double**) array1);
+
+    double a2[2] = {2, 1};
+    double a3[2] = {3, 4};
+    double *array2[2] = {a2, a3};
+    matrix_t *m2 = matrix_constructor(2, 2, (double**) array2);
+
+    matrix_t *m3 = matrix_allocator(1, 2);
+    matrix_multiply(m1, m2, m3);
+    free(m1);
+    free(m2);
+
+    EXPECT_EQ(m3->r, 1);
+    EXPECT_EQ(m3->c, 2);
+
+    EXPECT_EQ(m3->matrix[0][0], 8);
+    EXPECT_EQ(m3->matrix[0][1], 9);
+
+    matrix_free(m3);
+}
+
+TEST(matrix, matrix_multiply_scalar) {
+    double a0[3] = {1,2,3};
+    double a1[3] = {4,5,6};
+    double *array1[2] = {a0, a1};
+    matrix_t *m1 = matrix_constructor(2, 3, (double**) array1);
+    matrix_t *m2 = matrix_allocator(2, 3);
+    matrix_multiply_scalar(m1, 2, m2);
+
+    free(m1);
+
+    EXPECT_EQ(m2->r, 2);
+    EXPECT_EQ(m2->c, 3);
+
+    EXPECT_EQ(m2->matrix[0][0], 2);
+    EXPECT_EQ(m2->matrix[0][1], 4);
+    EXPECT_EQ(m2->matrix[0][2], 6);
+    EXPECT_EQ(m2->matrix[1][0], 8);
+    EXPECT_EQ(m2->matrix[1][1], 10);
+    EXPECT_EQ(m2->matrix[1][2], 12);
+
+    matrix_free(m2);
+}
+
+TEST(matrix, elementwise_multiply) {
+    double a0[2] = {1,0};
+    double a1[2] = {0,1};
+    double *array1[2] = {a0, a1};
+    matrix_t *m1 = matrix_constructor(2, 2, (double**) array1);
+
+    double a2[2] = {2, 1};
+    double a3[2] = {3, 4};
+    double *array2[2] = {a2, a3};
+    matrix_t *m2 = matrix_constructor(2, 2, (double**) array2);
+
+    matrix_t *m3 = matrix_allocator(2, 2);
+    matrix_elementwise_multiply(m1, m2, m3);
+    free(m1);
+    free(m2);
+
+    EXPECT_EQ(m3->r, 2);
+    EXPECT_EQ(m3->c, 2);
+
+    EXPECT_EQ(m3->matrix[0][0], 2);
+    EXPECT_EQ(m3->matrix[0][1], 0);
+    EXPECT_EQ(m3->matrix[1][0], 0);
+    EXPECT_EQ(m3->matrix[1][1], 4);
+
+    matrix_free(m3);
+}
+
+TEST(matrix, add) {
+    double a0[2] = {1,0};
+    double a1[2] = {0,1};
+    double *array1[2] = {a0, a1};
+    matrix_t *m1 = matrix_constructor(2, 2, (double**) array1);
+
+    double a2[2] = {2, 1};
+    double a3[2] = {3, 4};
+    double *array2[2] = {a2, a3};
+    matrix_t *m2 = matrix_constructor(2, 2, (double**) array2);
+
+    matrix_t *m3 = matrix_allocator(2, 2);
+    matrix_add(m1, m2, m3);
+    free(m1);
+    free(m2);
+
+    EXPECT_EQ(m3->r, 2);
+    EXPECT_EQ(m3->c, 2);
+
+    EXPECT_EQ(m3->matrix[0][0], 3);
+    EXPECT_EQ(m3->matrix[0][1], 1);
+    EXPECT_EQ(m3->matrix[1][0], 3);
+    EXPECT_EQ(m3->matrix[1][1], 5);
+
+    matrix_free(m3);
+}
+
+TEST(matrix, add_row) {
+    double a0[2] = {1,0};
+    double a1[2] = {0,1};
+    double *array1[2] = {a0, a1};
+    matrix_t *m1 = matrix_constructor(2, 2, (double**) array1);
+
+    double a2[2] = {2, 1};
+    double a3[2] = {3, 4};
+    double *array2[2] = {a2, a3};
+    matrix_t *m2 = matrix_constructor(2, 2, (double**) array2);
+
+    matrix_t *m3 = matrix_allocator(2, 2);
+    matrix_add_row(m1, 0, m2, 1, m3, 0);
+    free(m1);
+    free(m2);
+
+    EXPECT_EQ(m3->r, 2);
+    EXPECT_EQ(m3->c, 2);
+
+    EXPECT_EQ(m3->matrix[0][0], 4);
+    EXPECT_EQ(m3->matrix[0][1], 4);
+
+    matrix_free(m3);
+}
+
+TEST(matrix, sub) {
+    double a0[2] = {1,0};
+    double a1[2] = {0,1};
+    double *array1[2] = {a0, a1};
+    matrix_t *m1 = matrix_constructor(2, 2, (double**) array1);
+
+    double a2[2] = {2, 1};
+    double a3[2] = {3, 4};
+    double *array2[2] = {a2, a3};
+    matrix_t *m2 = matrix_constructor(2, 2, (double**) array2);
+
+    matrix_t *m3 = matrix_allocator(2, 2);
+    matrix_sub(m1, m2, m3);
+    free(m1);
+    free(m2);
+
+    EXPECT_EQ(m3->r, 2);
+    EXPECT_EQ(m3->c, 2);
+
+    EXPECT_EQ(m3->matrix[0][0], -1);
+    EXPECT_EQ(m3->matrix[0][1], -1);
+    EXPECT_EQ(m3->matrix[1][0], -3);
+    EXPECT_EQ(m3->matrix[1][1], -3);
+
+    matrix_free(m3);
+}
+
+TEST(matrix, column_extend) {
+    // todo
+}
+
+TEST(matrix, row_extend) {
+    // todo
+}
+
+TEST(matrix, transpose) {
+    double a0[3] = {1,2,3};
+    double a1[3] = {4,5,6};
+    double *array1[2] = {a0, a1};
+    matrix_t *m1 = matrix_constructor(2, 3, (double**) array1);
+
+    matrix_t *m2 = matrix_allocator(3, 2);
+    matrix_transpose(m1, m2);
+    free(m1);
+
+    EXPECT_EQ(m2->r, 3);
+    EXPECT_EQ(m2->c, 2);
+
+    EXPECT_EQ(m2->matrix[0][0], 1);
+    EXPECT_EQ(m2->matrix[0][1], 4);
+    EXPECT_EQ(m2->matrix[1][0], 2);
+    EXPECT_EQ(m2->matrix[1][1], 5);
+    EXPECT_EQ(m2->matrix[2][0], 3);
+    EXPECT_EQ(m2->matrix[2][1], 6);
+
+    matrix_free(m2);
+}
+
+TEST(matrix, equal) {
+    double a0[2] = {2,1};
+    double a1[2] = {3,4};
+    double *array1[2] = {a0, a1};
+    matrix_t *m1 = matrix_constructor(2, 2, (double**) array1);
+
+    double a2[2] = {2, 1};
+    double a3[2] = {3, 4};
+    double *array2[2] = {a2, a3};
+    matrix_t *m2 = matrix_constructor(2, 2, (double**) array2);
+
+    EXPECT_EQ(matrix_equal(m1, m2), true);
+
+    m1->matrix[0][0] = -2;
+    EXPECT_EQ(matrix_equal(m1, m2), false);
+
+    free(m1);
+    free(m2);
+}
+
+TEST(matrix, for_each_operator) {
     // todo
 }
